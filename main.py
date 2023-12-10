46522f324f1c61040e2abdfdaff4fbb8ab7b3a12
Ekalma Toto Alloy Sembiring <totoekalma@gmail.com>
2023-12-11 02:01:10 +0700

    Fiks Sekali, sudah dapat menampilkan grafik dan menambahkan validation loss, dan validation accuraccy training dan menampilkannya di log output

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+# Analisis Gambar Angka dan Operasi Hitung sebagai Media Perkembangan Anak menggunakan metode CNN
 # Mengimpor library yang diperlukan, termasuk OpenCV (cv2) untuk manipulasi gambar, NumPy untuk operasi array, TensorFlow untuk pembuatan dan pelatihan model jaringan saraf, dan Matplotlib untuk menampilkan gambar.
 
 print("Selamat datang di Pengenalan Angka Tulisan Tangan oleh Kelompok EkaVirMalaTia")
@@ -26,7 +26,12 @@ if train_new_model:
 
     model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-    model.fit(X_train, y_train, epochs=5)
+
+    class PrintMetricsCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs=None):
+            print("Epoch {}: Validation Loss: {:.4f}, Validation Accuracy: {:.4f}".format(epoch + 1, logs['val_loss'], logs['val_accuracy']))
+
+    model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test), callbacks=[PrintMetricsCallback()])
 
     val_loss, val_acc = model.evaluate(X_test, y_test)
     print(val_loss)
@@ -34,6 +39,7 @@ if train_new_model:
 
     model.save('handwritten_digits.model')
     # Jika train_new_model adalah True, maka model akan dilatih menggunakan dataset MNIST. Model yang dibangun memiliki dua lapisan tersembunyi dengan fungsi aktivasi ReLU dan satu lapisan output dengan fungsi aktivasi softmax. Model kemudian dikompilasi dengan optimizer 'adam' dan fungsi loss 'sparse_categorical_crossentropy'. Model yang dilatih dievaluasi pada set data uji, dan jika model baru, model tersebut disimpan sebagai file 'handwritten_digits.model'.
+
 else:
     model = tf.keras.models.load_model('handwritten_digits.model')
     # Jika train_new_model adalah False, model yang sudah dilatih sebelumnya akan dimuat.
@@ -58,11 +64,14 @@ while os.path.isfile('digits/digit{}.png'.format(nomor_gambar)):
         print("Hasil penambahan {} + {} adalah {}".format(angka_pertama, angka_kedua, hasil_penambahan))
         # Menggunakan loop while, program memproses gambar angka secara berurutan (digit1.png, digit2.png, dst.) dari direktori 'digits'. Setiap gambar dibaca menggunakan OpenCV, diproses, dan hasilnya diprediksi menggunakan model jaringan saraf. Hasil prediksi digunakan untuk menampilkan angka pertama dan kedua, dan hasil penjumlahannya.
 
+
+
         plt.imshow(img1[0], cmap=plt.cm.binary)
         plt.show()
         plt.imshow(img2[0], cmap=plt.cm.binary)
         plt.show()
         # Menampilkan gambar angka pertama dan angka kedua menggunakan Matplotlib. Gambar angka ditampilkan dalam skala abu-abu biner. Selanjutnya, hasil penjumlahan dari kedua angka ditampilkan.
+
 
         nomor_gambar += 2
         # Program melompat dua gambar untuk mengambil angka berikutnya. Hal ini dilakukan karena setiap iterasi loop while memproses dua gambar sekaligus.
@@ -71,10 +80,46 @@ while os.path.isfile('digits/digit{}.png'.format(nomor_gambar)):
         nomor_gambar += 1
         # Menangani error yang mungkin terjadi saat membaca gambar. Program tetap melanjutkan ke gambar berikutnya jika terjadi error.
 
-# Convolution
-# ReLu
-# Max Polling
-# Fatten
-# Fully Connected Nodes
-# Read out
-# Softmax
+        class PrintMetricsCallback(tf.keras.callbacks.Callback):
+            def __init__(self):
+                self.train_loss = []
+                self.train_acc = []
+                self.val_loss = []
+                self.val_acc = []
+
+            def on_epoch_end(self, epoch, logs=None):
+                print(
+                    "Epoch {}: Validation Loss: {:.4f}, Validation Accuracy: {:.4f}".format(epoch + 1, logs['val_loss'],
+                                                                                            logs['val_accuracy']))
+                self.train_loss.append(logs['loss'])
+                self.train_acc.append(logs['accuracy'])
+                self.val_loss.append(logs['val_loss'])
+                self.val_acc.append(logs['val_accuracy'])
+
+
+        # Ganti callback saat memanggil model.fit
+        print_metrics_callback = PrintMetricsCallback()
+        model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test), callbacks=[print_metrics_callback])
+
+        # Setelah pelatihan selesai, gunakan nilai yang telah disimpan untuk membuat grafik
+        epochs = range(1, len(print_metrics_callback.train_loss) + 1)
+
+        plt.figure(figsize=(12, 6))
+
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs, print_metrics_callback.train_loss, 'r', label='Training Loss')
+        plt.plot(epochs, print_metrics_callback.val_loss, 'b', label='Validation Loss')
+        plt.title('Training and Validation Loss')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs, print_metrics_callback.train_acc, 'r', label='Training Accuracy')
+        plt.plot(epochs, print_metrics_callback.val_acc, 'b', label='Validation Accuracy')
+        plt.title('Training and Validation Accuracy')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+
+        plt.show()
